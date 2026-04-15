feffc4ccacc18298518a09af03ea9b92791d1f17
jaspreetkr619 <jaspreetkr619@gmail.com>
2026-04-15 21:48:17 +0530

    Added main test program

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-// Main program entry point
+#include <iostream>
+#include "core/File.h"
+#include "core/Directory.h"
+
+using namespace std;
+
+int main() {
+    Directory root("root");
+
+    File file1("file1.txt", 10);
+    file1.write("Hello World");
+    root.addFile(file1);
+
+    Directory docs("docs");
+    root.addDirectory(docs);
+
+    File file2("file2.txt", 20);
+    file2.write("This is inside docs folder");
+    docs.addFile(file2);
+
+    root.listContents();
+    docs.listContents();
+
+    return 0;
+}
