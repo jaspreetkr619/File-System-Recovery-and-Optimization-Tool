64e22b8ba5bfc734b949e1d0492aaa09b7131089
jaspreetkr619 <jaspreetkr619@gmail.com>
2026-04-15 21:42:17 +0530

    Added File class declaration

--- core/File.h
+++ core/File.h
@@ -1,1 +1,21 @@
-// File class declaration
+#ifndef FILE_H
+#define FILE_H
+
+#include <string>
+using namespace std;
+
+class File {
+private:
+    string name;
+    int size;
+    string content;
+
+public:
+    File(string name, int size);
+    void write(string data);
+    string read() const;
+    string getName() const;
+    int getSize() const;
+};
+
+#endif
