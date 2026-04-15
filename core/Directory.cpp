4261c6e7c8d08db15180f8bd3385ba5591365b86
jaspreetkr619 <jaspreetkr619@gmail.com>
2026-04-15 21:47:06 +0530

    Added Directory class implementation

--- core/Directory.cpp
+++ core/Directory.cpp
@@ -1,1 +1,29 @@
-// Directory class implementation
+#include "Directory.h"
+
+Directory::Directory(string name) {
+    this->name = name;
+}
+
+void Directory::addFile(File file) {
+    files.push_back(file);
+}
+
+void Directory::addDirectory(Directory directory) {
+    subdirectories.push_back(directory);
+}
+
+void Directory::listContents() const {
+    cout << "\nDirectory: " << name << endl;
+
+    for (const auto &file : files) {
+        cout << "- " << file.getName() << endl;
+    }
+
+    for (const auto &dir : subdirectories) {
+        cout << "[DIR] " << dir.getName() << endl;
+    }
+}
+
+string Directory::getName() const {
+    return name;
+}
