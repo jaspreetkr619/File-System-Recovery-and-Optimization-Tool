aa74139197945606feb10fe87e7c4a86e9b1b31c
jaspreetkr619 <jaspreetkr619@gmail.com>
2026-04-15 21:43:56 +0530

    Added File class implementation

--- core/File.cpp
+++ core/File.cpp
@@ -1,1 +1,23 @@
-// File class implementation
+#include "File.h"
+
+File::File(string name, int size) {
+    this->name = name;
+    this->size = size;
+    this->content = "";
+}
+
+void File::write(string data) {
+    content += data;
+}
+
+string File::read() const {
+    return content;
+}
+
+string File::getName() const {
+    return name;
+}
+
+int File::getSize() const {
+    return size;
+}
