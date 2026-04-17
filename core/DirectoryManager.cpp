99b265af55fc49b7ad4d1ce7253f838a8a1b7edd
Vinay Kothari <vvkothari0046@gmail.com>
2026-04-18 00:41:33 +0530

    Enhance DirectoryManager with storage allocation
    Added storage management for files and implemented deleteFile method.

--- core/DirectoryManager.cpp
+++ core/DirectoryManager.cpp
@@ -2,7 +2,8 @@
 #include<iostream>
 using namespace std;
 
-DirectoryManager::DirectoryManager() : root("root"), current("root") {}
+DirectoryManager::DirectoryManager(int totalBlocks)
+    : root("root"), current("root"), storage(totalBlocks) {}
 
 void DirectoryManager::createFolder(const std::string& name) {
     Directory newDir(name);
@@ -10,10 +11,20 @@ void DirectoryManager::createFolder(const std::string& name) {
     cout<< "Folder created: "<<name<<endl;
 }
 
-void DirectoryManager::createFile(const std::string& name,int size) {
-    File newFile(name, size);
-    current.addFile(newFile);
-    cout << "File created: " <<name<<endl;
+void DirectoryManager::createFile(const std::string& name, int size, int blocks) {
+    if (storage.allocateBlocks(blocks)) {
+        File newFile(name,size);
+        current.addFile(newFile);
+        cout << "File created: " <<name<<endl;
+    } else {
+        cout << "Not enough storage space\n";
+    }
+}
+
+void DirectoryManager::deleteFile(const std::string& name, int blocks) {
+    current.deleteFile(name);
+    storage.freeBlocks(blocks);
+    cout << "Storage freed for file: " <<name<<endl;
 }
 
 void DirectoryManager::searchFile(const std::string& name) const {
