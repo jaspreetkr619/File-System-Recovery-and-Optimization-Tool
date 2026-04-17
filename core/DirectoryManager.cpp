757a654d9af565e60b59fc45df29c6d6e5de8269
Vinay Kothari <vvkothari0046@gmail.com>
2026-04-17 09:44:16 +0530

    Add file creation and search functionality

--- core/DirectoryManager.cpp
+++ core/DirectoryManager.cpp
@@ -10,6 +10,16 @@ void DirectoryManager::createFolder(const std::string& name) {
     cout<< "Folder created: "<<name<<endl;
 }
 
+void DirectoryManager::createFile(const std::string& name,int size) {
+    File newFile(name, size);
+    current.addFile(newFile);
+    cout << "File created: " <<name<<endl;
+}
+
+void DirectoryManager::searchFile(const std::string& name) const {
+    current.searchFile(name);
+}
+
 void DirectoryManager::listContents() const {
     current.listContents();
 }
