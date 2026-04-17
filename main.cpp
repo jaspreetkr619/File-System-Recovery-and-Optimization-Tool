c84cd792c36cc58c872f711d7224ff3c4b4d173d
jaspreetkr619 <jaspreetkr619@gmail.com>
2026-04-17 12:02:08 +0530

    Integrated StorageManager into main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,46 @@
 #include <iostream>
 #include "core/File.h"
 #include "core/Directory.h"
+#include "core/StorageManager.h"
 
 using namespace std;
 
 int main() {
-    // Step 1: Initialize storage
+
     int totalSpace = 100;
     int usedSpace = 0;
-    
+
+    StorageManager storage(10);
+
     Directory root("root");
 
-    // Create file1
     File file1("file1.txt", 10);
     file1.write("Hello World");
     root.addFile(file1);
-
-    // Track space
     usedSpace += file1.getSize();
+    storage.allocateBlocks(2);
 
-    // Create directory
     Directory docs("docs");
     root.addDirectory(docs);
 
-    // Create file2
     File file2("file2.txt", 20);
     file2.write("This is inside docs folder");
     docs.addFile(file2);
-
-    // Track space
     usedSpace += file2.getSize();
+    storage.allocateBlocks(3);
 
-    // Delete file1
     root.deleteFile("file1.txt");
+    usedSpace -= file1.getSize();
+    storage.freeBlocks(2);
 
-    //Search files
     root.searchFile("file1.txt");
-    root.searchFile("file2.txt");
+    docs.searchFile("file2.txt");
 
-    // Display contents
     root.listContents();
     docs.listContents();
 
-    // Show free space
     cout << "Free Space: " << totalSpace - usedSpace << endl;
+    storage.displayStorageStatus();
 
     return 0;
 }
