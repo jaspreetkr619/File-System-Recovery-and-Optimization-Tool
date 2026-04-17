91bedd689de2e2bb381489cd94993836f08fb9bd
Vinay Kothari <vvkothari0046@gmail.com>
2026-04-18 00:37:19 +0530

    Add StorageManager and update DirectoryManager constructor

--- core/DirectoryManager.h
+++ core/DirectoryManager.h
@@ -2,18 +2,22 @@
 #define DIRECTORY_MANAGER_H
 
 #include "Directory.h"
+#include "File.h"
+#include "StorageManager.h"
 #include<string>
 
 class DirectoryManager {
 private:
     Directory root;
     Directory current;
+    StorageManager storage;
 
 public:
-    DirectoryManager();
+    DirectoryManager(int totalBlocks);
 
     void createFolder(const std::string& name);
     void createFile(const std::string& name,int size);
+    void deleteFile(const std::string& name,int blocks);
     void searchFile(const std::string& name) const;
     void listContents() const;
 };
