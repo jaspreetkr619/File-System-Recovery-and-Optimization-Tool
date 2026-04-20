5e20c1800aecf34ad0ea1f974c324ce5d4293cda
VarnikaSharma2176 <varnikasharma2006@gmail.com>
2026-04-20 15:31:00 +0530

    feat: integrate cache system with main workflow

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "logger.h" //allows the main.cpp to use logger
 #include "recovery.h"
+#include "cache.h"
 using namespace std;
 
 int main() {
@@ -11,6 +12,18 @@ int main() {
     //Transaction-2 Crash Simulation
     writeLog("TXN 2 START WRITE file2 data456");
     cout << "Simulating crash..." << endl;
+    cout << "\n--- Cache Simulation ---\n";
+
+    // Store data in cache
+    putCache("file1", "data123");
+    putCache("file2", "data456");
+
+    // Retrieve data
+    getCache("file1");   // should be HIT
+    getCache("file3");   // should be MISS
+
+    // Show all cache
+    showCache();
     cout << "\n--- Recovery Phase ---\n";
     recoverSystem();
     return 0;
