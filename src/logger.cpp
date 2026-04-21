edc1a95750d9592dc4a57c5b8166d6c71b3fd3a0
VarnikaSharma2176 <varnikasharma2006@gmail.com>
2026-04-21 12:34:18 +0530

    feat: add timestamp to log entries for better readability

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,13 +1,21 @@
 #include "logger.h"
 #include <fstream> // allows to work with files
 #include <iostream>
+#include <ctime>
 using namespace std;
 void writeLog(string message)
 {
     ofstream logFile("../data/logs.txt", ios::app);//open in append mode
     if(logFile.is_open())
     {
-        logFile << message << endl;//writes th message into the file
+        time_t now = time(0);
+        char* dt = ctime(&now);
+
+        // remove newline from ctime
+        string timeStr(dt);
+        timeStr.pop_back();
+
+        logFile << "[" << timeStr << "] " << message << endl;
         logFile.close();//closes the file
     }
     else{
