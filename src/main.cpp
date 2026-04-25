3086e27a784bd1205a303a11af1d7b70aee52da9
VarnikaSharma2176 <varnikasharma2006@gmail.com>
2026-04-25 19:53:38 +0530

    refactor: convert CLI menu system to command-based execution for backend integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,67 @@
 #include <iostream>
+#include <string>
 #include "logger.h"
 #include "recovery.h"
 #include "cache.h"
-int txnCounter = 1;
+
 using namespace std;
 
-int main() {
-    int choice;
+int txnCounter = 1;
 
-    while (true) {
-        cout << "\n=================================\n";
-        cout << "     FILE SYSTEM SIMULATOR\n";
-        cout << "=================================\n";
-        cout << "1. Perform Transaction\n";
-        cout << "2. Simulate Crash\n";
-        cout << "3. Run Recovery\n";
-        cout << "4. Show Cache\n";
-        cout << "5. Exit\n";
-        cout << "---------------------------------\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-        if (choice == 1) {
-            string file, data;
+int main(int argc, char* argv[]) {
 
-            cout << "Enter file name: ";
-            cin >> file;
+    if (argc < 2) {
+        cout << "No command provided.\n";
+        return 1;
+    }
 
-            cout << "Enter data: ";
-            cin >> data;
+    string command = argv[1];
 
-            int txnId = txnCounter++;
+    if (command == "transaction" && argc == 4) {
+        string file = argv[2];
+        string data = argv[3];
 
-            string startLog = "TXN " + to_string(txnId) + " START WRITE " + file + " " + data;
-            writeLog(startLog);
+        int txnId = txnCounter++;
 
-            putCache(file, data);
+        string startLog = "TXN " + to_string(txnId) + " | START | WRITE " + file + " " + data;
+        writeLog(startLog);
 
-            string commitLog = "TXN " + to_string(txnId) + " | COMMIT";
-            writeLog(commitLog);
+        putCache(file, data);
 
-           cout << "[SUCCESS] Transaction committed.\n";
-        }
-        if (choice == 2) {
-            string file, data;
+        string commitLog = "TXN " + to_string(txnId) + " | COMMIT";
+        writeLog(commitLog);
 
-            cout << "Enter file name: ";
-            cin >> file;
+        cout << "[SUCCESS] Transaction completed.\n";
+    }
 
-            cout << "Enter data: ";
-            cin >> data;
+    else if (command == "crash" && argc == 4) {
+        string file = argv[2];
+        string data = argv[3];
 
-           int txnId = txnCounter++;
+        int txnId = txnCounter++;
 
-            string startLog = "TXN " + to_string(txnId) + " | START | WRITE " + file + " " + data;
-            writeLog(startLog);
+        string startLog = "TXN " + to_string(txnId) + " | START | WRITE " + file + " " + data;
+        writeLog(startLog);
 
-            cout << "[WARNING] Crash simulated. Transaction not committed.\n";
-        }
-        if (choice == 3) {
-            cout << "\n--- Running Recovery ---\n";
-            recoverSystem();
-        }
-        if (choice == 4) {
-            showCache();
-        }
-        if (choice == 5) {
-            cout << "Exiting...\n";
-            break;
-        }
+        cout << "[WARNING] Crash simulated. No commit.\n";
+    }
+
+    else if (command == "recovery") {
+        recoverSystem();
+    }
+
+    else if (command == "cache") {
+        showCache();
+    }
+
+    else if (command == "clear") {
+        ofstream logFile("../data/logs.txt", ios::trunc);
+        logFile.close();
+        cout << "[INFO] Logs cleared.\n";
+    }
+
+    else {
+        cout << "Invalid command.\n";
     }
 
     return 0;
